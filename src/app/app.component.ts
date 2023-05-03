9dfe3ae3a1299f6d68078d769a3712d67d7c0cd5
Albin Aj <albinattumalil@gmail.com>
2023-05-03 11:14:45 +0530

    list  commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'todoList';
+
+  taskArray:any=[]
+  inputTask: any;
+  visible?:boolean
+  completeArray:any=[]
+i: any;
+
+
+  addTask(formContent: any): void {
+    this.taskArray.push(formContent.value.task)
+    // console.log(formContent);
+    formContent.reset()
+  // console.log(this.taskArray);
+    // console.log(formContent.value ='value');
+  }
+
+  onComplete(i:number) {
+     this.visible=!this.visible
+     let complete =this.taskArray.splice(i,1)
+     this.completeArray?.push(complete)
+      console.log(this.completeArray);
+  }
+
+  onDelete(i: any) {
+    this.completeArray.splice(i,1)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MaterialModuleModule } from './material-module/material-module.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './component/header/header.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModuleModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
